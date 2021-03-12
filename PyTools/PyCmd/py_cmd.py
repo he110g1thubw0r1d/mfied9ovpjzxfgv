c0e9463a79a2248b62eee82ed0f0dbd8b9ad22de
koloramax <65012343+koloramax@users.noreply.github.com>
2021-03-12 16:04:48 +0900

    Update py_cmd.py

--- PyTools/PyCmd/py_cmd.py
+++ PyTools/PyCmd/py_cmd.py
@@ -1,4 +1,4 @@
-# PyCmd a210312
+# PyCmd v1.0-alpha.1(a210312)
 uppercase = [
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
