37967db3925da3fe58dcb4a6e962fa2a0e599bfd
koloramax <65012343+koloramax@users.noreply.github.com>
2021-03-16 17:14:45 +0900

    Update py_cmd.py

--- PyTools/PyCmd/py_cmd.py
+++ PyTools/PyCmd/py_cmd.py
@@ -1,9 +1,9 @@
 # PyCmd v1.0-alpha.2(a210316)
 from time import sleep
 uppercase = [
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-        ]
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
 files = {}
 working_dir = None
 directory_count = None
@@ -38,6 +38,8 @@ def rm(command):
         print("Use 'rmdir' command to remove a directory.")
         return
     del working_dir[argument]
+
+
 def cd(command):
     global location
     argument = command[3:]
@@ -69,84 +71,6 @@ def add_disk():
     global files, disk_list
     argument = input("Enter disk name(Only one uppercase letter allowed): ")
     if (argument in uppercase) and (len(argument) == 1):
-        if argument in disk_list:
-            print(f"The disk '{argument}' is already in the list.")
-        else:
-            disk_list.append(argument)
-            files[argument + ":\\"] = {"DIR_COUNT": 0}
-    else:
-        print("Invalid disk name")
-# PyCmd v1.0-alpha.2(a210316)
-from time import sleep
-upper_case = [
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-        ]
-files = {}
-working_dir = None
-directory_count = None
-disk_list = []
-location = None
-
-
-def ls(command):
-    directory_count = working_dir["DIR_COUNT"]
-    print(
-        f"{(len(working_dir)-1)-directory_count} Files, {directory_count} Directories"
-    )
-    if (len(working_dir) - 1) != 0:
-        print(" ".join(working_dir.keys())[10:])
-
-
-def touch(command):
-    global files, location
-    argument = command[6:]
-    if "." not in argument:
-        argument += ".txt"
-    working_dir[argument] = ""
-
-
-def rm(command):
-    global working_dir
-    argument = command[3:]
-    if argument not in working_dir.keys():
-        print("Invalid file")
-        return
-    if dict(working_dir[argument]):
-        print("Use 'rmdir' command to remove a directory.")
-        return
-    del working_dir[argument]
-def cd(command):
-    global location
-    argument = command[3:]
-    if (argument == "..") and (location[1:] != ":\\"):
-        location = "\\".join(location.split("\\")[:-1])
-        return
-    if argument not in (working_dir.keys()):
-        print("Invalid directory")
-        return
-    if location[1:] != ":\\":
-        location += "\\"
-    location += argument
-
-
-def rmdir(command):
-    global files, location, working_dir
-    argument = command[6:]
-    if (argument not in working_dir.keys()):
-        print("Invalid directory")
-        return
-    if not dict(working_dir[argument]):
-        print("Use 'rm' command to remove a file.")
-        return
-    del working_dir[argument]
-    files[location]["DIR_COUNT"] -= 1
-
-
-def add_disk():
-    global files, disk_list
-    argument = input("Enter disk name(Only one upper_case letter allowed): ")
-    if (argument in upper_case) and (len(argument) == 1):
         if argument in disk_list:
             print(f"The disk '{argument}' is already in the list.")
         else:
@@ -192,7 +116,7 @@ def open_disk(disk_name):
 
 def remove_disk(disk):
     global files, disk_list
-    if (disk in upper_case) and (len(disk) == 1):
+    if (disk in uppercase) and (len(disk) == 1):
         if disk not in disk_list:
             print(f"The disk '{disk}' is not in the list.")
         else:
@@ -207,7 +131,9 @@ def run():
     file = open("data.txt", "r")
     username = file.readline()[:-1]
     if username == "(null)":
-        print("Error: No user\nOpen data.txt and replace (null) with your username.")
+        print(
+            "Error: No user\nOpen data.txt and replace (null) with your username."
+        )
         sleep(15)
         return
     if file.readline()[-5:-1] == "True":
@@ -230,7 +156,7 @@ def run():
         action_list = [["add", "ad", "1"], ["open", "od", "2"],
                        ["remove", "rd", "3"], ["show_list", "sdl", "4"],
                        ["quit", "saq", "5"]]
-        
+
         for i in range(len(action_list)):
             if action in action_list[i]:
                 action_index = i
@@ -242,7 +168,7 @@ def run():
             add_disk()
         elif action_index == 1:
             argument = input("Enter disk name: ")
-            if (argument in upper_case) and (len(argument) == 1):
+            if (argument in uppercase) and (len(argument) == 1):
                 if argument in disk_list:
                     open_disk(argument)
                 else:
